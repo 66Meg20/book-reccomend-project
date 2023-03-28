f36bb2edc297d0d3fa251e1837b109bb446c1c3e
66Meg20 <66meg.dj2066@gmail.com>
2023-03-27 23:17:11 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> Books</h1>
+      <div className="container">
+        <h1> Books</h1>
+        <footer>
+          This project was coded by Megan Jackson and is {""}
+          <a
+            href="https://github.com/66Meg20/book-reccomend-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
