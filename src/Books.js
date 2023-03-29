037e372074029b24e474274c40fa6bdcaa150a48
66Meg20 <66meg.dj2066@gmail.com>
2023-03-29 00:03:56 -0500

    imported useState and working on the api integration

--- src/Books.js
+++ src/Books.js
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Books.css";
 
 export default function Books() {
+  const [ready, setReady] = useState(false);
+  const [title, setTitle] = useState(null);
   function handleResponse(response) {
     console.log(response.data);
+    setTitle(response.data.docs[0].title);
+    setReady(true);
   }
 
-  let apiUrl = `https://openlibrary.org/search.json?q=the+lord+of+the+rings`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="Books">
-      {" "}
-      <h1>Throne of Glass</h1>
-      <form>
-        <input type="search" placeholder="book name" />
-        <button className="btn btn-primary"> Search </button>
-      </form>
-      <ul>
-        <li> A court of thorns and roses</li>
-        <li> From blood and ash</li>
-        <li> Cresent city</li>
-      </ul>
-    </div>
-  );
+  if (ready) {
+    return (
+      <div className="Books">
+        {" "}
+        <h1>{title}</h1>
+        <form>
+          <input type="search" placeholder="book name" />
+          <button className="btn btn-primary"> Search </button>
+        </form>
+        <ul>
+          <li> A court of thorns and roses</li>
+          <li> From blood and ash</li>
+          <li> Cresent city</li>
+        </ul>
+      </div>
+    );
+  } else {
+    let apiUrl = `https://openlibrary.org/search.json?q=from+blood+and+ash`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
