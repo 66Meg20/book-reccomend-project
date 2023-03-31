4dfce9ee83e5fc8a7fd5fdead9d9fd1da77ac218
66Meg20 <66meg.dj2066@gmail.com>
2023-03-30 23:51:17 -0500

    added data to the page

--- src/Books.js
+++ src/Books.js
@@ -6,12 +6,13 @@ import "./Books.css";
 export default function Books() {
   const [bookData, setBookData] = useState({ ready: false });
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data.docs[0].top_work);
 
     setBookData({
       ready: true,
       title: response.data.docs[0].title,
       author_name: response.data.docs[0].author_name,
+      top_work: response.data.docs[0].top_work,
     });
   }
 
@@ -30,11 +31,15 @@ export default function Books() {
           <li> Cresent city</li>
         </ul>
         <h2>{bookData.author_name}</h2>
+        <ul>
+          <li>{bookData.top_work}</li>
+          <li>a shadow in the ember</li>
+        </ul>
       </div>
     );
   } else {
-    let apiUrl = `https://openlibrary.org/search.json?title=throne+of+glass`;
-    let authorUrl = `https://openlibrary.org/search/authors.json?q=${bookData.author_name}`;
+    let apiUrl = `https://openlibrary.org/search.json?title=from blood and ash`;
+    let authorUrl = `https://openlibrary.org/search/authors.json?q=jennifer l armentrout`;
     axios.get(apiUrl).then(handleResponse);
     axios.get(authorUrl).then(handleResponse);
 
